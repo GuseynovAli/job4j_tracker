a063ce74c1fe4978cffebac74ec3ccce45e5905f
guseynov <guseynov.smm@gmail.com>
2023-05-17 12:59:13 +0500

    Truck

---
+++ src/main/java/ru/job4j/oop/Vehicle.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public abstract class Vehicle {
+    private String brand;
+    private String model;
+    private String type;
+    private int passengers;
+    private int maxSpeed;
+
+    public abstract void accelerate();
+
+    public abstract void steer();
+
+    public abstract void brake();
+}
